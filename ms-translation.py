b1ae0d865829f350ea6916c4c18fbd8271977ada
Jens-Fabian Goetzmann <mail@jefago.com>
2017-10-16 11:36:51 +0200

    Working prototype

--- ms-translation.py
+++ ms-translation.py
@@ -2,9 +2,36 @@
 
 import os
 import sys
-# import requests
+from xml.etree import ElementTree
+
+import requests
+
+TRANSLATE_URI = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&from={}&to={}"
+LANGUAGES = [
+    #"en-US", 
+    "de-DE"
+    # "ar", "de-DE", "en-AU", "en-CA", "en-GB", "es-419", "es-ES", "es-US", "fr-CA", "fr-FR",
+    # "hr", "it-IT", "ja-JP", "ko-KR", "nl-NL", "pl-PL", "pt-BR", "pt-PT", "ru-RU", "sr", "zh-CN"
+]
 
 
+def translate(api_key, from_lang, text_to_translate):
+    headers = {
+        "Ocp-Apim-Subscription-Key": api_key
+    }
+
+    for to_lang in LANGUAGES:
+        uri_to_call = TRANSLATE_URI.format(text_to_translate, "en-us", to_lang)
+        response = requests.get(uri_to_call, headers=headers)
+
+        print response.text
+
+        translation_xml_tree = ElementTree.fromstring(response.text.encode('utf-8'))
+
+        print to_lang
+        print translation_xml_tree.text
+
+        print u"{}: {}".format(to_lang, translation_xml_tree.text)
 
 
 
@@ -16,7 +43,11 @@ if __name__ == "__main__":
         print "Missing environment variable MS_TRANSLATION_API_KEY"
         exit(1)
 
-    if len(sys.argv) < 2:
-        print "Syntax: {} [text to translate]".format(sys.argv[0])
+    arg_list = list(sys.argv)
+    del arg_list[0]
 
-    print "Main"
+    if len(arg_list) < 1:
+        print "Syntax: {} [text to translate]".format(sys.argv[0])
+        exit(2)
+
+    translate(API_KEY, "en-us", ' '.join(arg_list))
