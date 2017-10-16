f69f535035f9e8c8ec54d521256c1a0a48f664df
Jens-Fabian Goetzmann <mail@jefago.com>
2017-10-16 11:49:26 +0200

    Working translation script

--- ms-translation.py
+++ ms-translation.py
@@ -9,28 +9,38 @@ import requests
 TRANSLATE_URI = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&from={}&to={}"
 LANGUAGES = [
     "en-US", 
-    "de-DE"
-    # "ar", "de-DE", "en-AU", "en-CA", "en-GB", "es-419", "es-ES", "es-US", "fr-CA", "fr-FR",
-    # "hr", "it-IT", "ja-JP", "ko-KR", "nl-NL", "pl-PL", "pt-BR", "pt-PT", "ru-RU", "sr", "zh-CN"
+    "ar", "de-DE", "en-AU", "en-CA", "en-GB", "es-419", "es-ES", "es-US", "fr-CA", "fr-FR",
+    "hr", "it-IT", "ja-JP", "ko-KR", "nl-NL", "pl-PL", "pt-BR", "pt-PT", "ru-RU", "sr", "zh-CN"
 ]
 
 
 def translate(api_key, from_lang, text_to_translate):
+    # Headers contain API key
     headers = {
         "Ocp-Apim-Subscription-Key": api_key
     }
 
+    # Dictionary of translations to return
+    retval = {}
+
+    # Iterate over all languages to translate
     for to_lang in LANGUAGES:
+
+        # Include original copy if this is the language we are translating from
         if to_lang == from_lang:
             translation = text_to_translate
         else:
+            # MS Translation API request
             uri_to_call = TRANSLATE_URI.format(text_to_translate, from_lang, to_lang)
             response = requests.get(uri_to_call, headers=headers)
 
             translation_xml_tree = ElementTree.fromstring(response.text.encode('utf-8'))
             translation = translation_xml_tree.text
+        # Add translation to dictionary that we will return
+        retval[to_lang] = translation
 
-        print u"<{0}>\n{1}\n</{0}>\n".format(to_lang, translation)
+    # Return the dictionariy of translations
+    return retval
 
 if __name__ == "__main__":
     API_KEY = os.environ.get('MS_TRANSLATION_API_KEY')
@@ -46,4 +56,7 @@ if __name__ == "__main__":
         print "Syntax: {} [text to translate]".format(sys.argv[0])
         exit(2)
 
-    translate(API_KEY, "en-us", ' '.join(arg_list))
+    translations = translate(API_KEY, "en-us", ' '.join(arg_list))
+
+    for lang in translations:
+        print u"<{0}>\n{1}\n</{0}>\n".format(lang, translations[lang])
